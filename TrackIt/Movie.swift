c1f87095b27099a342903ebea3d528e5f7214db0
Student11 <student11@Imac3s-Mac-mini.local>
2024-08-18 14:35:29 +0300

    movie added

---
+++ TrackIt/Movie.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  TrackIt
+//
+//  Created by Student11 on 18/08/2024.
+//
+
+import Foundation
+
+class Movie{
+    var title: String
+    var releaseDate: String
+    var overview: String
+    var imageURL: String
+    var runtime: Int //duration in minutes
+    
+    init(title: String, releaseDate: String, overview: String, imageURL: String, runtime: Int) {
+        self.title = title
+        self.releaseDate = releaseDate
+        self.overview = overview
+        self.imageURL = imageURL
+        self.runtime = runtime
+    }
+    
+}
