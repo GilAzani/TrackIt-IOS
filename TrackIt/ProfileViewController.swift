8e71f95a329ef2abf17d479e9e59eb33ddc92656
Student11 <student11@Imac3s-Mac-mini.local>
2024-08-18 13:29:42 +0300

    favorite move cell added

---
+++ TrackIt/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  TrackIt
+//
+//  Created by Student11 on 18/08/2024.
+//
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var favoriteMoviesTableView: UITableView!
+    @IBOutlet weak var numberOfMoviesLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TrackIt/SimpleMovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleMovieCell.swift
+//  TrackIt
+//
+//  Created by Student11 on 18/08/2024.
+//
+
+import UIKit
+
+class SimpleMovieCell: UITableViewCell {
+
+    @IBOutlet weak var movieNameLabel: UIView!
+    
+    @IBOutlet weak var movieImageView: UIView!
+    
+}
