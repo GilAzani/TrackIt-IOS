6cde014ce2ef1e8c89e839b4fddfdad9b3c57099
Student11 <student11@Imac3s-Mac-mini.local>
2024-08-22 20:32:06 +0300

    added bracket to create default list

--- TrackIt/User.swift
+++ TrackIt/User.swift
@@ -8,7 +8,7 @@
 class User{
     var username: String
     var userImage: String // url to user image
-    var movieList: [MovieListItem]
+    var movieList: [MovieListItem] = []
     
     init(username: String, userImage: String, movieList: [MovieListItem]) {
         self.username = username
